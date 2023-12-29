73dbd15f0b3ab529e53bb16492a742d231c579eb
Bruce Tushabe <brucetushabe@gmail.com>
2023-12-29 22:55:27 +0300

    Coding fire goes on

--- try.py
+++ try.py
@@ -1,10 +1,42 @@
-# Initialize an empty dictionary
-user_dict = {}
+from getpass import getpass as input
 
-# Get user input for key-value pairs
-user_dict["name"] = input("Enter your name: ")
-user_dict["age"] = int(input("Enter your age: "))  # Convert to an integer
-user_dict["city"] = input("Enter your city: ")
+import pyfiglet
+font = pyfiglet.figlet_format("EPIC GAME")
+print(font)
 
-# Display the dictionary
-print("User Dictionary:", user_dict)
+print ("Welcome to Epic Game")
+
+print("Select your move(R,P, S)")
+print()
+
+
+# Input validation for Player1 
+while True:
+  player1move = input("Player1 move> ")
+  if player1move in ["R", "P", "S"]:
+    break
+  else:
+    print("Invalid move. Please enter R, P, or S")
+print()
+
+# Input validation for Player2
+while True:
+  player2move = input("Player2 move>")
+  if player2move in ["R", "P", "S"]:
+    break
+  else:
+    print("Invalid move. Please enter R,P, or S")
+print()
+
+# Game Logic
+
+if player1move == player2move:
+  print("Wow, its a tie!")
+
+elif (player1move == "R" and player2move == "S") or (player1move == "P" and player2move == "R") or (player1move == "S" and player2move == "P"):
+  print(f"Player1 wins! {player1move} beats {player2move}")
+else:
+  print(f"Player 2 wins! {player2move} beats {player1move}")
+
+
+  
