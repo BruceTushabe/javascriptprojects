4fb033c535cdea4b9e2f81a7ae438641e21a704e
Bruce Tushabe <brucetushabe@gmail.com>
2023-12-31 23:37:35 +0300

    lets do this as we end 2023

---
+++ happyny.py
@@ -0,0 +1,18 @@
+import pyfiglet
+from termcolor import cprint
+from colorama import Fore
+font= pyfiglet.figlet_format("Happy New Year 2024")
+cprint(font, 'red')
+
+
+def heart_shape(msg=" Happy New Year 2024"):
+    lines = []
+    for y in range(15, -15, -1):
+        line = ""
+
+        for x in range(-30, 30):
+            f = ((x * 0.05) ** 2 + (y * 0.1) **2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) **3
+            line += msg[(x - y) % len(msg)] if f <= 0 else " "
+        lines.append(line)
+    print(Fore.RED+ "\n".join(lines))
+heart_shape()
