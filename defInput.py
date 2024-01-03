a2bf2aed53fa077ea6230527acaca9c30be201e4
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-03 15:09:23 +0300

    We continue to do amazing things

--- defInput.py
+++ defInput.py
@@ -12,3 +12,16 @@ userBase = input("Name the base: ")
 userCoating = input("Name the coating: ")
 
 whichCake(userIngredient, userBase, userCoating)
+
+
+def pizza_order(topping1, topping2):
+  if topping1 == "pepperoni":
+    print(topping1, "is a great choice")
+  else:
+    print(topping1, "is kinda lame on pizza")
+    print(topping2, "sounds delicious, much better than", topping1)
+
+topping1 =  input("Name a pizza topping. ")
+topping2 = input("Name a second pizza topping. ")
+
+pizza_order(topping1, topping2)
