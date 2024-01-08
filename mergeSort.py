5b6406bf8c82ebd2566ea23097d4b2eea04113ca
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-09 00:16:32 +0300

    We continue to  build

--- mergeSort.py
+++ mergeSort.py
@@ -3,18 +3,19 @@
 
 def merge_sort(arr):
     if len(arr) > 1:
-        mid = len(arr) // 2
+        mid = len(arr)//2
         left = arr[:mid]
         right = arr[mid:]
 
-        # Recursively sort the left and right halves
+        # Recursively sort the left and right havlves
+
         merge_sort(left)
         merge_sort(right)
 
-        # Merge the sorted halves
+        # Merge the sorted havlves
         i = j = k = 0
         while i < len(left) and j < len(right):
-            if left[i] <= right[j]:
+            if left[i] < right [j]:
                 arr[k] = left[i]
                 i += 1
             else:
@@ -22,20 +23,23 @@ def merge_sort(arr):
                 j += 1
             k += 1
 
-        # Copy remaining elements from left if any
+        # Copy remaining elements of left half if any
         while i < len(left):
             arr[k] = left[i]
             i += 1
             k += 1
 
-        # Copy remaining elements from right if any
+        # Copy remaining elements of right half if any
         while j < len(right):
             arr[k] = right[j]
             j += 1
             k += 1
 
-# Example usage
-arr = [64, 34, 25, 12, 22, 11, 90]
-print("Unsorted Array:", arr)
+
+# Example usage 
+arr = [8, 3, 5, 9, 1, 4, 6, 7, 2]
+print("Unsorted array: ", arr)
 merge_sort(arr)
-print("Sorted array:", arr)
+print("Sorted array: ", arr)
+
+        
