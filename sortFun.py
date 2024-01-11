5d441173f9325e1aadf6a723e464c7fdba2163e8
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-11 07:33:48 +0300

    we continue to code

---
+++ sortFun.py
@@ -0,0 +1,18 @@
+
+# sort() - This sorts and changes the array
+
+
+numbers = [8,6,4,2,1,7,3,5]
+
+numbers.sort()
+
+print(numbers)
+
+
+# sorted() - this sorts but does not change the original 
+
+numbers = (8,6,4,2,1,7,3,5)
+
+sortedNumbers = sorted(numbers)
+
+print(sortedNumbers)
