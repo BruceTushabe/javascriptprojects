119c0d9a7e5b1119b159d70ab495830ae8db5909
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-12 23:56:49 +0300

    Lets do it

--- merge2.py
+++ merge2.py
@@ -9,7 +9,36 @@ def mergSot(arr):
        mergSot(patArray2)
 
 
-       # 
+       # initial values for pointers
+
+       i = j = k = 0
+
+       while i < len(patArray1) and j < len(patArray2):
+           if patArray1[i] < patArray2[j]:
+               arr[k] = patArray1[i]
+               i += 1
+        
+               
+            else:
+               arr[k] = patArray2[j]
+               j += 1
+            k += 1
+           
+        while i < len(patArray1):
+           arr[k] = patArray1[i]
+           i += 1
+           k += 1
+
+        while j < len(patarray2):
+            arr[k] = patArray2[j]
+            j += 1
+            k += 1
+           
+
+
+
+
+
 
 
 
