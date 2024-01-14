072e35c4639813f3d253adcd7d926a3c7987f768
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-14 13:46:20 +0300

    Lets commit again

--- merge2.py
+++ merge2.py
@@ -1,48 +1,37 @@
-def mergSot(arr):
+def magi(arr):
     if len(arr) > 1:
+        mid = len(arr) // 2
+        lefthalf = arr[:mid]
+        righthalf = arr[mid:]
 
-       mid = len(arr)//2
-       patArray1 = arr[:mid]
-       patArray2 = arr[mid:]
+        magi(lefthalf)
+        magi(righthalf)
 
-       mergSot(patArray1)
-       mergSot(patArray2)
+        i = j = k = 0
 
-
-       # initial values for pointers
-
-       i = j = k = 0
-       
-       while i < len(patArray1) and j < len(patArray2):
-        if patArray1[i] < patArray2[j]:
-            arr[k] = patArray1[i]
+    while i < len(lefthalf) and j < len(righthalf):
+        if lefthalf[i] < righthalf[j]:
+            arr[k] = lefthalf[i]
             i += 1
-        else:
-            arr[k] = patArray2[j]
-            j += 1
-        k += 1
-           
-    while i < len(patArray1):
-        arr[k] = patArray1[i]
+            k += 1
+
+    while i < len(righthalf):
+        arr[k] = lefthalf[i]
         i += 1
         k += 1
-           
-    while j < len(patArray2):
-        arr[k] = patArray2[j]
-        j += 1
-        k += 1
 
-# using
+        # Copy remaining elements of right half if any
+    while j < len(righthalf):
+      arr[k] = righthalf[j]
+      j += 1
+      k += 1
+
+
+# Example usage 
+arr = [8, 3, 5, 9, 1, 4, 6, 7, 2]
+print("Unsorted array: ", arr)
+magi(arr)
+print("Sorted array: ", arr)
+
         
-arr = [10, 9, 2, 4, 6, 13]
-mergSot(arr)
-print(arr)
-           
-
-
-
-
-
-
-
 
---
+++ testarr.py
@@ -0,0 +1,35 @@
+def magi(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        lefthalf = arr[:mid]
+        righthalf = arr[mid:]
+
+        magi(lefthalf)
+        magi(righthalf)
+
+        i = j = k = 0
+
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                arr[k] = lefthalf[i]
+                i += 1
+            else:
+                arr[k] = righthalf[j]
+                j += 1
+            k += 1
+
+        while i < len(lefthalf):
+            arr[k] = lefthalf[i]
+            i += 1
+            k += 1
+
+        while j < len(righthalf):
+            arr[k] = righthalf[j]
+            j += 1
+            k += 1
+
+# Example usage
+arr = [8, 3, 5, 9, 1, 4, 6, 7, 2]
+print("Unsorted array: ", arr)
+magi(arr)
+print("Sorted array: ", arr)
