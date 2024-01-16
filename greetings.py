b7f790f6fe8dd13c318d01d99f3741207f3a6a21
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-16 16:20:51 +0300

    Greetings code

--- greetings.py
+++ greetings.py
@@ -1,1 +1,9 @@
 # Greetings
+
+import random
+
+greeting = ["Agandi", "Namasate", "kopango", "amahoro", "Gyebale", "Bonjour","Hola", "Zdravstvuyte", "nin hao"]
+
+index = random.randint(0, 9)
+
+print(f"Yo! {greeting[index]}.")
