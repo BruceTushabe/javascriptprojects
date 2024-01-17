6b71255de48e4cdbf14627695194eecd9678f020
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-17 20:39:21 +0300

    Randomly coding again

---
+++ deJurny.py
@@ -0,0 +1,45 @@
+myPartyList = []
+def printList():
+    for item in myPartyList:
+        print(item)
+while True:
+    menu = input(""""Choose what I should do:
+                 1 - Add a person to the party list
+                 2 - Remove a person from the Party list
+                 3 - Print the Party List
+                 4 - Exit
+                 Enter your choice here: """)
+    
+    if menu == "1":
+        item = input("Who should I add to the Party List?: ")
+        if item:
+            myPartyList.append(item)
+        else:
+            print("You need to enter a name!")
+
+    elif menu == "2":
+        item = input("Who should I remove from the Party List?: ")
+
+        if item in myPartyList:
+            myPartyList.remove(item)
+
+        else:
+
+            print(f"{item} is not on the list! ")
+    
+    elif menu == "3":
+        printList()
+
+    elif menu == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("That is not a valid choice!")
+        print("Please choose 1, or 2, or 3")
+
+
+print("Exiting guest list management system.")
+
+
+
--- testarr.py
+++ testarr.py
@@ -2,13 +2,10 @@ import time
 
 print ("30 DAYS DOWN")
 
-
-
 for i in range(1, 20):
     print (f"Day {i} ") 
     thought = input()
     time.sleep(1)
    
-
     print(f"You thought Day {i} was", thought)
 
