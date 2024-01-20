b9b17309906058a86b0817da1a3b87ffe764f7eb
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-20 14:40:17 +0300

    Doing it again with Binary

--- yoBinary.py
+++ yoBinary.py
@@ -1,38 +1,37 @@
 class Node:
-    # Constructor to create a new node 
-
-    def __init__ (self, data):
-        self.data = data
+    def __init__(self, data):
         self.left = None
         self.right = None
+        self.data = data
 
 
+
+# Compute the "maxDepth" of a tree -- the number of nodes
+# along the longest path from the root node down to the
+# farthest leaf node
+        
 def maxDepth(node):
     if node is None:
         return 0
+    
     else:
-
-        # Compute the depth of each subtree
+        # Compute depth of each subtree
 
         leftDepth = maxDepth(node.left)
         rightDepth = maxDepth(node.right)
 
         # Use the larger one
+
         if (leftDepth > rightDepth):
             return leftDepth + 1
-        
         else:
             return rightDepth + 1
         
-        # Driver code to test the above function
-
+# Driver code to test the above code
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
-root.right.left = Node(5)
+root.left.right = Node(5)
 
-
-print ("Height of tree is %d" %(maxDepth(root)))
-
-        
+print("Height of the tree %d" % (maxDepth(root)))
