eca0d05186e91a0291075c9ae3f57335542405fc
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-21 08:53:33 +0300

    BINARY SEARCH IN ACTION

--- yoBinary.py
+++ yoBinary.py
@@ -1,35 +1,67 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self):
+        self.key = 0
         self.left = None
         self.right = None
-        self.data = data
 
-# Compute the "maxDepth" of a tree -- the number of nodes
-# along the longest path from the root node down to the
-# farthest leaf node
+# Utility function to create a new node
         
-def maxDepth(node):
-    if node is None:
-        return 0
-    
-    else:
-        # Compute depth of each subtree
+def newNode(node):
 
-        leftDepth = maxDepth(node.left)
-        rightDepth = maxDepth(node.right)
+    temp = Node()
+    temp.key = node
+    temp.left, temp.right = None, None
+    return temp
 
-        # Use the larger one
+# Function to find the height(depth) of the tree
+def height(root):
+    depth = 0
 
-        if (leftDepth > rightDepth):
-            return leftDepth + 1
-        else:
-            return rightDepth + 1
-        
-# Driver code to test the above code
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
+    q = []
 
-print("Height of the tree %d" % (maxDepth(root)))
+    # Appending first level element along with None
+
+    q.append(root)
+    q.append(None)
+
+    while (len(q) > 0):
+        temp = q[0]
+        q = q[1:]
+
+        #When None encountered, incrementthe value
+        if(temp == None):
+            depth += 1
+
+        # If None not encountered, keep moving
+        if(temp != None):
+            if(temp.left):
+                q.append(temp.left)
+
+            if(temp.right):
+                q.append(temp.right)
+
+    # If queue still have elements left,
+        # append None again to the queue.
+                
+        elif(len(q) > 0):
+            q.append(None)
+
+    return depth
+
+# Driver program
+# Let us create Binary Tree shown in above example
+
+root = newNode(1)
+root.left = newNode(2)
+root.right = newNode(3)
+root.left.left = newNode(4)
+root.left.right = newNode(5)
+
+print(f"Height(Depth) of tree is: {height(root)}")
+
+
+
+
+
+
+
