b1e1506f808a718e61ed7df2cc02c959bd8de734
Bruce Tushabe <brucetushabe@gmail.com>
2024-01-29 23:57:19 +0300

    Dealing with APIs in python

--- apisTry.py
+++ apisTry.py
@@ -2,13 +2,13 @@ import requests
 
 import json
 
+# Dealing with APIs in python
 
 response = requests.get("http://api.open-notify.org/astros.json")
 
 print(response.json())
 
 print(response.status_code)
-
 
 def jprint(obj):
     text = json.dumps(obj, sort_keys= True, indent = 4)
