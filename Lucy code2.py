104ddd42d27b50f615a86c4761f5dc8fe2a4d2c0
Bruce Tushabe <brucetushabe@gmail.com>
2024-02-05 23:47:16 +0300

    .

---
+++ Lucy code2.py
@@ -0,0 +1,10 @@
+print('a life with God')
+God=input('Do you know how to walk with God')
+if God=='yes':
+    print('you live with grace')
+    print('everything you do just flows because Gods blessings are abundant upon your life')
+    print('his grace is sufficient for you')
+    print('all things work out for the good of those that believe in him')
+    
+else:
+    print('Try walking with God,seek him')
---
+++ lucy code.py
@@ -0,0 +1,13 @@
+print('God is amazing')
+God = input('isnt God amazing?')
+if God== 'yes':
+    print('there is beauty in knowing God')
+    print('God is not a man he never lies')
+    print('his word comes to pass')
+    print('he is a God of confirmation')
+else:
+    print('vanity of vanities')
+    print('in God we find life, we are lifeless without God')
+    print('in the beginning was the word and the word was with God and the word was God')
+    print('God is the word')
+    print('God is everything beautiful, seek him and you will find him')
